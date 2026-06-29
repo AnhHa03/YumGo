64e9579ff0094ed670a50951bb2fb8461f219aa5
Hà Mai Anh <114716577+AnhHa03@users.noreply.github.com>
2026-06-29 12:37:34 +0700

    refactor: move MaterialApp to app layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:yumgo/features/home/home_screen.dart';
+import 'package:yumgo/app/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: HomeScreen());
-  }
 }
